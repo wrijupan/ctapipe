75f8e9693ee325e2ed0b117437fc1365348531f7
Christoph Deil <Deil.Christoph@gmail.com>
2015-07-08 13:16:58 +0100

    Update gh-pages docs

--- reco/shower_example.py
+++ reco/shower_example.py
@@ -2,25 +2,39 @@
 """
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import pylab as plt
 from ctapipe.io.camera import make_rectangular_camera_geometry
-from ctapipe.reco import shower_model, make_mock_shower_image
+from ctapipe.reco import generate_2d_shower_model, make_mock_shower_image
 
-geom = make_rectangular_camera_geometry()
+NX = 40
+NY = 40
 
-showermodel = shower_model(centroid=[0.25, 0.0], length=0.1,
+geom = make_rectangular_camera_geometry(NX, NY)
+
+showermodel = generate_2d_shower_model(centroid=[0.25, 0.0], length=0.1,
                            width=0.02, psi=np.radians(40))
 
 image, signal, noise = make_mock_shower_image(geom, showermodel.pdf,
                                               intensity=20, nsb_level_pe=30)
 
+# make them into 2D arrays so we can plot them with imshow
+image.shape = (NX, NY)
+signal.shape = (NX, NY)
+noise.shape = (NX, NY)
+
+# here we just plot the images using imshow().  For a more general
+# case, one should use a ctapipe.visualization.CameraDisplay
 plt.figure(figsize=(10, 3))
 plt.subplot(1, 3, 1)
-plt.imshow(signal, interpolation='none')
+plt.imshow(signal, interpolation='nearest', origin='lower')
+plt.title("Signal")
 plt.colorbar()
 plt.subplot(1, 3, 2)
-plt.imshow(noise, interpolation='none')
+plt.imshow(noise, interpolation='nearest', origin='lower')
+plt.title("Noise")
 plt.colorbar()
 plt.subplot(1, 3, 3)
-plt.imshow(image, interpolation='none')
+plt.imshow(image, interpolation='nearest', origin='lower')
+plt.title("Image")
 plt.colorbar()
+
+#plt.show()
